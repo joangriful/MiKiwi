755a9ac3b94408ff955fd1e01de8a5db10146eb0
savamidev <migue.626@gmail.com>
2026-01-26 14:34:48 +0100

    feat: Enhance Review model with UUIDs, factory support, mass assignment properties, attribute casting, and relationships to User and Product.

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -3,8 +3,31 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Review extends Model
 {
-    //
+    use HasFactory, HasUuids;
+
+    protected $fillable = [
+        'rating',
+        'comment',
+        'is_approved',
+    ];
+
+    protected $casts = [
+        'rating' => 'integer',
+        'is_approved' => 'boolean',
+    ];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+    
+    public function product() {
+        return $this->belongsTo(Product::class);
+    }
+
+
 }
