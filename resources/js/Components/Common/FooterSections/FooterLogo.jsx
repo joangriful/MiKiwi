8b08b35a24c1922ef28c3285ffc2c0c5e18edd34
Ismael Vargas <ismaelvargasduque14@gmail.com>
2026-02-23 10:45:56 +0100

    Change - Footer modificado, funcional 100%

--- resources/js/Components/Common/FooterSections/FooterLogo.jsx
+++ resources/js/Components/Common/FooterSections/FooterLogo.jsx
@@ -1,10 +1,10 @@
+import MikiwiLogo from '@/Components/MikiwiLogo';
+
 export default function FooterLogo() {
     return (
-        <div className="flex-1 mb-10 md:mb-0">
-            <img
-                src="/assets/icons/mikiwi_logo.svg"
-                alt="MiKiwi Logo"
-                className="w-64 md:w-72 h-auto invert brightness-0 opacity-90"
+        <div className="flex-1 mb-10 md:mb-0 flex justify-start">
+            <MikiwiLogo
+                className="w-48 md:w-64 h-auto text-white opacity-90 transition-transform hover:scale-105 duration-300"
             />
         </div>
     );
