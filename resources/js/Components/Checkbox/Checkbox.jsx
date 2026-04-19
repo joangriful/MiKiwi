7114efbe73af8d634ff5ce80552b919b3cecbdb8
Ángel Jiménez Ragel <jimenezragelangel@gmail.com>
2026-04-19 12:38:57 +0200

    refactor: CheckBox

--- resources/js/Components/Checkbox/Checkbox.jsx
+++ resources/js/Components/Checkbox/Checkbox.jsx
@@ -5,10 +5,7 @@ export default function Checkbox({ className = '', ...props }) {
         <input
             {...props}
             type="checkbox"
-            className={
-                `${styles.root} rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500 ` +
-                className
-            }
+            className={[styles.root, className].filter(Boolean).join(' ')}
         />
     );
 }
