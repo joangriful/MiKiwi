25e97fa384756d954d00b224bbb45522e6795fa2
savamidev <migue.626@gmail.com>
2026-04-22 17:53:29 +0200

    feat: add lazy loading for Mannequin 3D viewer and scene components

---
+++ resources/js/Components/Configurator/utils/lazyLoaders 2.js
@@ -0,0 +1,5 @@
+export const loadMannequin3DViewer = () =>
+    import('@/Components/Configurator/Mannequin3DViewer/Mannequin3DViewer');
+
+export const loadMannequinScene3D = () =>
+    import('@/Components/Configurator/Mannequin3DViewer/components/MannequinScene3D');
