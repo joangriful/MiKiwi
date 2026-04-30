bb33d595caddbdf1e1b98228de11e0d91b003e93
Ángel Jiménez Ragel <jimenezragelangel@gmail.com>
2026-04-30 20:58:37 +0200

    renombrar UserAddressFactory a AddressFactory

--- database/factories/AddressFactory.php
+++ database/factories/AddressFactory.php
@@ -4,14 +4,17 @@ declare(strict_types=1);
 
 namespace Database\Factories;
 
+use App\Models\Address;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\UserAddress>
+ * @extends Factory<Address>
  */
-class UserAddressFactory extends Factory
+class AddressFactory extends Factory
 {
+    protected $model = Address::class;
+
     /**
      * Define the model's default state.
      *
