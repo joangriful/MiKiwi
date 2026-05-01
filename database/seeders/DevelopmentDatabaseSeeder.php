32440fd888dcf22455e9a4e5f2e377b60f95ad0b
Ángel Jiménez Ragel <jimenezragelangel@gmail.com>
2026-05-01 22:12:28 +0200

    ajusto seeder

--- database/seeders/DevelopmentDatabaseSeeder.php
+++ database/seeders/DevelopmentDatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DevelopmentDatabaseSeeder extends Seeder
@@ -22,6 +23,11 @@ class DevelopmentDatabaseSeeder extends Seeder
             UserSeeder::class,
             ProductSeeder::class,
         ]);
+
+        if (User::count() < 10) {
+            User::factory()->count(20)->customer()->create();
+        }
+
         $this->command->newLine();
 
         $this->command->info('📍 Fase 3: Creando histórico y soporte demo...');
